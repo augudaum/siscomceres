1480ebeb84481d559609dd0581bde62f6e0f72df
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2018-03-12 22:59:10 -0300

    Métodos para ativação/inativação do usuário adicionados

--- app/models/usuarios/Usuario.php
+++ app/models/usuarios/Usuario.php
@@ -11,4 +11,30 @@
         // Identifica os dados de um usuário Administrador
         public $session_id_string = 'usuario_id';
         const SEQUENCE = "usuarios_id_seq";
+
+        /**
+         * Ativa um usuário
+         * @param $id = ID do usuário a ser ativado
+         */
+        public function ativarUsuario($id) {
+            $sql = "UPDATE {$this->table} SET ativo = 1 WHERE id = :id";
+            $pdo = $this->database->prepare($sql);
+            $pdo->bindValue(':id', $id);
+            $pdo->execute();
+
+            return $pdo->rowCount();
+        }
+
+        /**
+         * Inativa um usuário
+         * @param $id = ID do usuário a ser inativado
+         */
+        public function inativarUsuario($id) {
+            $sql = "UPDATE {$this->table} SET ativo = 0 WHERE id = :id";
+            $pdo = $this->database->prepare($sql);
+            $pdo->bindValue(':id', $id);
+            $pdo->execute();
+
+            return $pdo->rowCount();
+        }
     }
