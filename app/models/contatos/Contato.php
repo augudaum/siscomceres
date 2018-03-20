b2a926588c1c6aef68e1a3bd20acb4db4d186d79
AfQMorais <afqmorais@gmail.com>
2018-03-20 10:46:23 -0300

    Alteração dos atributos e referência de classes associadas

--- app/models/contatos/Contato.php
+++ app/models/contatos/Contato.php
@@ -3,9 +3,11 @@
     namespace app\models\contatos;
 
     use app\models\database\DefaultModel;
+    use app\models\contatos\Telefone;
+    use app\models\contatos\Email;
 
     class Contato extends DefaultModel {
         private $id;
-        private $tipoTelefone;
+        private $telefone;
         private $email;
     }
