f432a34f0d1a43a58d21403331949f12f8c7cf26
AfQMorais <afqmorais@gmail.com>
2018-04-04 19:54:32 -0300

    Criação das classes de produtos

---
+++ app/models/produtos/Marca.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     * Classe responsável por modelar a marca
+     * @author Alanfagner
+     */
+
+    namespace app\models\produtos;
+
+    use app\models\database\DefaultModel;
+
+    class Marca extends DefaultModel {
+        private $id;
+        private $descricao;
+
+        /**
+         * GET and SET methods
+         */ 
+        
+    }
---
+++ app/models/produtos/UnidadeComercial.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+     * Classe responsável por modelar o unidade comercial
+     * @author Alanfagner
+     */
+
+    namespace app\models\produtos;
+
+    use app\models\database\DefaultModel;
+
+    class UnidadeComercial extends DefaultModel {
+        private $id;
+        private $descricao;
+        private $sigla;
+
+        /**
+         * GET and SET methods
+         */ 
+        
+    }
