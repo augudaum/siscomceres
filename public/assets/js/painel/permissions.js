49d2fa5462564a21fc0c4261eb3e180a41fb54a0
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2018-04-18 20:28:44 -0300

    Correção da sintaxe na chamada do sweetalert

--- public/assets/js/painel/permissions.js
+++ public/assets/js/painel/permissions.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 	var button_permission = $('.button-permission');
 
-	button_permission.on('click', function(event) {
+	button_permission.on('click', function (event) {
 		event.preventDefault();
 
 		var data = $(this).attr('data-action');
 
 		$.ajax({
 			url: '/permissoes/update',
-			data: 'data='+data,
+			data: 'data=' + data,
 			type: 'post',
-			success: function(response) {
+			success: function (response) {
 				if (response == 'atualizado') {
 					swal({
-						title: 'Atualização realizada', 
-						text: 'As permissões foram alteradas.', 
-						type: 'success',
-						confirmButtonText: 'OK'
-					}, function() {
+						title: 'Atualização realizada',
+						text: 'As permissões foram alteradas.',
+						icon: 'success',
+						button: 'OK'
+					}).then(() => {
 						location.reload();
 					});
 				} else {
