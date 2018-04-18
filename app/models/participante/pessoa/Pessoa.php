cfaca57b758cb0b5685bd4c7747d7992d863e5bc
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2018-04-18 20:34:54 -0300

    Configurando instanciação do tipo de Pessoa

--- app/models/participante/pessoa/Pessoa.php
+++ app/models/participante/pessoa/Pessoa.php
@@ -14,19 +14,19 @@
         // Variável responsável por determinar Pessoa Física ou Jurídica
         private $tipo;
 
-        // public function __construct($tipo) {
-        //     $this->tipo = $tipo;
-        //     //$this->instantiateTipoPessoa();
-        // }
-
         public function getTipo() {
             return $this->tipo;
+        }
+
+        public function setTipo($tipo) {
+            $this->tipo = $tipo;
         }
 
         /**
          * @return Pessoa retorna o tipo de pessoa a ser criada
          */
         public function instantiateTipoPessoa($tipo) {
+            $this->setTipo($tipo);
             switch (strtoupper($tipo)) {
                 case 'F':
                     return new PessoaFisica();
