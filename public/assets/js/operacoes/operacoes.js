1edc2799cacd404813fecdc8261478e245f0547e
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2018-05-09 10:48:46 -0300

    Adição de produto requisitado na tabela

--- public/assets/js/operacoes/operacoes.js
+++ public/assets/js/operacoes/operacoes.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
     var button_addrequisicao = $('#button-addrequisicao');
     var button_addpedido = $('#button-addpedido');
+    var button_addtotable = $('#button-addprodutototable');
+    var form_addrequisicao = $('#addRequisicaoForm');
+
+    setFormRequisicaoValidate();
 
     // Abre o modal para cadastrar uma requisição
     button_addrequisicao.on('click', (event) => {
@@ -10,6 +14,23 @@ $(document).ready(() => {
     // Abre o modal para montar um pedido de compra
     button_addpedido.on('click', (event) => {
         $('#addPedidoModal').modal('show');
+    });
+
+    // Adiciona o produto e a quantidade à tabela de requisições
+    button_addtotable.on('click', (event) => {
+        if (form_addrequisicao.form('is valid')) {
+            var codigo = $('input[name=codigo_produto]').val();
+            var quantidade = $('input[name=quantidade]').val();
+            var descricao = $('#dropdown-produtos').dropdown('get text');
+            $('table#table-produtos-requisicao tbody').append(
+                $('<tr>').attr('data-id', codigo).append(
+                    $('<td>').html(descricao),
+                    $('<td>').html(quantidade)
+                )
+            );
+        } else {
+            form_addrequisicao.form('validate form');
+        }
     });
 
     // Inicializa o dropdown de fornecedores
@@ -40,3 +61,48 @@ $(document).ready(() => {
         }
     });
 });
+
+function initDropdowns() {
+    $.ajax({
+        url: '/Produtos/produtos',
+        type: 'POST',
+        data: { action: 'request' },
+        dataType: 'json',
+        success: function (response, status, request) {
+            for (var i = 0; i < response.length; i++) {
+                $('#dropdown-produtos .menu').append(
+                    $('<div>').addClass('item').attr('data-value', (response[i].codigo != "" ? response[i].codigo : response[i].cean)).html(response[i].descricao)
+                );
+            }
+            $('#dropdown-produtos').dropdown({
+                context: '#addRequisicaoModal'
+            });
+        }
+    });
+}
+
+function setFormRequisicaoValidate() {
+    $('#addRequisicaoForm').form({
+        fields: {
+            quantidade: {
+                identifier: 'quantidade',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Informe a quantidade desejada'
+                    }
+                ]
+            },
+            codigo_produto: {
+                identifier: 'codigo_produto',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Selecione um produto'
+                    }
+                ]
+            }
+        },
+        inline: true
+    });
+}
