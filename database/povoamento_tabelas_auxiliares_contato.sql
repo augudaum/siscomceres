e613a47bdb4a5e9597b32d2126d1556c4383da46
AfQMorais <afqmorais@gmail.com>
2018-05-16 09:47:14 -0300

    Tabelas povoamento contatos

---
+++ database/povoamento_tabelas_auxiliares_contato.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_categorias_contato (categoria) VALUES
+('comercial'),
+('facebook'),
+('instagram'),
+('pessoal'),
+('profissional'),
+('residencial'),
+('twitter');
+
+INSERT INTO tb_tipo_contato (tipo) VALUES
+('email'),
+('fax'),
+('rede social'),
+('telefone celular'),
+('telefone fixo');
