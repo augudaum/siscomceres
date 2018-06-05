545b7020dc95b4aa5b76d1f5aaa7c51cc6d0764d
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2018-06-05 09:11:20 -0300

    Criação dos métodos show e store

--- app/controllers/RequisicoesController.php
+++ app/controllers/RequisicoesController.php
@@ -4,6 +4,7 @@
 
     use app\controllers\ContainerController;
     use app\models\compras\Requisicao;
+    use app\models\compras\ItemRequisicao;
 
     class RequisicoesController extends ContainerController {
 
@@ -19,6 +20,23 @@
                 'title' => 'Painel Administrativo :: Compras > Requisições',
                 'requisicoes' => $requisicoes
             ], 'operacoes.compras.requisicoes.index');
+        }
+
+        public function store() {
+            $numeroRequisicao = (new Requisicao())->create(array('data_requisicao' => request()->get()->data_requisicao, 'cadastrado_por' => $_SESSION['usuario_id']));
+            if ($numeroRequisicao) {
+                //return toJson(["requisicao" => $numeroRequisicao]);
+               
+                foreach ($_POST['produtos'] as $p) {
+                    (new ItemRequisicao())->create(array('numero_requisicao' => $numeroRequisicao, 'codigo_produto' => $p['codigo_produto'], 'quantidade' => $p['quantidade']));
+                }
+            }
+        }
+
+        public function show() {
+            // Retornar todos os produtos como arrays bidimensionais
+            $requisicao = (new ItemRequisicao())->findByInArray('numero_requisicao', request()->get()->id);
+            return toJson($requisicao);
         }
     }
 
